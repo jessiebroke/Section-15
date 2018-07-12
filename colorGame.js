9c46311fabe59999ba8a45b1a2e5b657563749f7
jessiesantiago <jsantia@g.clemson.edu>
2018-07-12 00:48:12 -0400

    Added pickColor

--- colorGame.js
+++ colorGame.js
@@ -8,8 +8,9 @@ var colors = [
 ]
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
@@ -23,10 +24,25 @@ squares[i].addEventListener("click", function(){
     var clickedColor = this.style.backgroundColor;
     //compare color to pickedColor
     if(clickedColor === pickedColor){
-        alert("Correct!");
+        messageDisplay.textContent = "Correct Color";
+        changeColors(clickedColor);
     } else {
-        alert("Wrong!");
+        this.style.backgroundColor = "#232323";
+        messageDisplay.textContent = "Try Again";
     }
 
 });
+}
+
+function changeColors(color){
+    //loop through all squares
+    for(var i = 0; i < squares.length; i++){
+    //change each color to match given color
+        squares[i].style.backgroundColor = color;
+    }
+}
+
+function pickColor(){
+    var random = Math.floor((Math.random() * colors.length));
+    return colors[random];
 }
