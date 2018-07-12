0d35f81d2b9371fba03f3a9162303bc95ef4c2cd
jessiesantiago <jsantia@g.clemson.edu>
2018-07-12 00:25:29 -0400

    Finished part 2

--- colorGame.js
+++ colorGame.js
@@ -8,7 +8,25 @@ var colors = [
 ]
 
 var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
+
+colorDisplay.textContent = pickedColor;
 
 for(var i = 0; i < squares.length; i++){
-    squares[i].style.backgroundColor = colors[i];
+//add initial colors to squares
+squares[i].style.backgroundColor = colors[i];
+
+//add click listeners to squares
+squares[i].addEventListener("click", function(){
+    //grab color of picked square
+    var clickedColor = this.style.backgroundColor;
+    //compare color to pickedColor
+    if(clickedColor === pickedColor){
+        alert("Correct!");
+    } else {
+        alert("Wrong!");
+    }
+
+});
 }
